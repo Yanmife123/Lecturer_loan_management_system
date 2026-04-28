de06f9c6c972ef1076da41047ed31eae93567698
Yanmife Adegbola Adesanya <149130655+Yanmife123@users.noreply.github.com>
2026-04-28 18:03:04 +0100

    Fix typo in environment variable name

--- src/lib/getImageUrl.ts
+++ src/lib/getImageUrl.ts
@@ -1,5 +1,5 @@
 export function ImageUrl() {
-  const baseUrl = process.env.NEXT_PUBLIC_API_Image;
+  const baseUrl = process.env.NEXT_PUBLIC_API_Imag;
   if (!baseUrl) {
     throw new Error(
       "NEXT_PUBLIC_API_Image environment variable is not defined",
