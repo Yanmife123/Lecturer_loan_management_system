525ab9021ea9716a556a957b84483095c960cb28
Yanmife Adegbola Adesanya <149130655+Yanmife123@users.noreply.github.com>
2026-06-07 13:14:02 +0100

    Update page.tsx

--- src/app/admin/dashboard/savings/add_saving_record/page.tsx
+++ src/app/admin/dashboard/savings/add_saving_record/page.tsx
@@ -8,7 +8,7 @@ export default function AddSaving() {
         title="Add Savings Record"
         description="Enter a member's monthly savings contribution to update their account balance."
         breadcrumbs={[
-          { label: "Dashboard", href: "/admin/dasboard/" },
+          { label: "Dashboard", href: "/admin/dashboard/" },
           {
             label: "Saving Record",
             href: "/admin/dashboard/savings",
